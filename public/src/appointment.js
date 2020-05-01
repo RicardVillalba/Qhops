793625750e624b882f0eda8b4218e4062b2c9c39
Alex AD <skanndar@gmail.com>
2020-05-01 13:53:57 +0200

    add src to public folder

--- public/src/appointment.js
+++ public/src/appointment.js
@@ -2,7 +2,7 @@ const tagsInput = document.querySelector("#tags-input");
 const tagsDisplay = document.querySelector("#tags");
 const tagsListHidden = document.querySelector("#tags-list");
 
-tagsInput.addEventListener("keypress", function(e) {
+tagsInput.addEventListener("keypress", function (e) {
   let inputText = e.target.value;
   // const lastTypedChar = inputText[inputText.length - 1];
 
@@ -11,7 +11,7 @@ tagsInput.addEventListener("keypress", function(e) {
     newLi.innerHTML = inputText;
     tagsDisplay.appendChild(newLi);
 
-    if (tagsListHidden.value ==="" ) tagsListHidden.value += inputText;
+    if (tagsListHidden.value === "") tagsListHidden.value += inputText;
     else tagsListHidden.value += " " + inputText;
 
     console.log("tagsListHidden.value", tagsListHidden.value);
@@ -21,6 +21,7 @@ tagsInput.addEventListener("keypress", function(e) {
     // req.body.tagsList.split(" ")
 
     e.target.value = "";
+    e.preventDefault()
   }
 });
 

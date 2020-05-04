01c56a013e049dfded0550fd610825dd9425d1ce
Alex AD <skanndar@gmail.com>
2020-05-04 23:18:49 +0200

    refactor all models, add room-model

--- models/appointment-model.js
+++ models/appointment-model.js
@@ -1,14 +1,13 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // CREATE SCHEMA
 const appointmentSchema = new Schema({
-    code: {type: String, required: true},
-    fName: {type: String, required: true},
-    lName: {type: String, required: true},
+    code: { type: String, required: true },
+    fName: { type: String, required: true },
+    lName: { type: String, required: true },
     tags: [String],
-    email: {type: String, required: true},
+    email: { type: String, required: true },
     isUrgent: Boolean,
     status: {
         type: String,
@@ -16,14 +15,13 @@ const appointmentSchema = new Schema({
         appointment_start_At: Date,
         appointment_finished_At: Date,
         required: true
-    }
+    },
+    room: { type: Schema.Types.ObjectId, ref: "Room" }
 });
-
 
 // CREATE MODEL
 //                                     Appointments
 const Appointment = mongoose.model('Appointment', appointmentSchema);
-
 
 // EXPORT
 module.exports = Appointment;
--- models/queue-model.js
+++ models/queue-model.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -7,20 +6,15 @@ const queueSchema = new Schema({
     appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
     inProgress: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
     appointments_done: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
-    roomId: String,
     nurseId: Schema.Types.ObjectId,
     date: Date,
     capacity: Number, //      ( numSpots*workingHours )
     patientsServed: Number,
     avgTime: Number, //   ( timepast / patients_Served )
-}
-);
-
-
+});
 // CREATE MODEL
 //                            queues
 const Queue = mongoose.model('Queue', queueSchema);
-
 
 // EXPORT
 module.exports = Queue;
